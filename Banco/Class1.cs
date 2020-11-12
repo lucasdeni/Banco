6a727ca4022c2b831b7b97c3eb5a2b0f1dc191d5
Lucas Denipoti <lucasfdenipoti@gmail.com>
2020-11-11 23:18:39 -0300

    código finalizado

---
+++ Banco/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Banco
+{
+    struct Conta
+    {
+        // Declarar variáveis
+        private int _numero;
+        public string _nome;
+        private double? _saldo;
+        public Conta(int numero, string nome, double? saldo) // Contrutor
+        {
+            _numero = numero;
+            _nome = nome;
+            _saldo = saldo;
+        }
+        public void Deposito(double quantidade) // Realizar deposito
+        {
+            double valor = _saldo ?? 0;
+            _saldo = valor + quantidade;
+        }
+        public void Saque(double quantidade) // Realizar saque
+        {
+            double valor = _saldo ?? 0;
+            _saldo = valor - quantidade - 5;
+        }
+
+        // Retornar o ToString dos dados
+        public override string ToString()
+        {
+            return "Dados da Conta: Conta " + _numero
+                + ", Titular: " + _nome
+                + ", Saldo R$ " + _saldo.GetValueOrDefault().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
--- Banco/Program.cs
+++ Banco/Program.cs
@@ -6,7 +6,37 @@ namespace Banco
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Informações da conta
+            Console.Write("Entre com o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Entre com o titular da conta: ");
+            string titular = Console.ReadLine();
+            Console.Write("Haverá depósito inicial (S/N): ");
+            string boolDeposito = Console.ReadLine();
+
+            // Deposito inicial
+            double? deposito = null;
+            if (boolDeposito == "S")
+            {
+                Console.Write("Entre o valor de depósito: ");
+                deposito = double.Parse(Console.ReadLine());
+            }
+
+            // Instanciar struct
+            Conta n = new Conta(numero, titular, deposito);
+            Console.WriteLine("\n" + n + "\n");
+
+            // Realizar depósito
+            Console.Write("Entre com um valor para depósito: ");
+            n.Deposito(double.Parse(Console.ReadLine()));
+            Console.WriteLine(n + "\n");
+
+            // Realizar saque
+            Console.Write("Entre com um valor para saque: ");
+            n.Saque(double.Parse(Console.ReadLine()));
+            Console.WriteLine(n);
+
+            Console.ReadKey();
         }
     }
 }
